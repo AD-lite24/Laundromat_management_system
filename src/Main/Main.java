d2b828c36b569f6b80a0125ef151a6a90150f49f
Aditya Dandwate <adityadandwate24@gmail.com>
2022-11-29 21:03:00 +0530

    removed body

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,15 +4,6 @@ import hostel.*;
 public class Main {
 	public static void main(String args[]) {
 
-		Hostel krishna = new Hostel("Krishna");
-		Hostel gandhi = new Hostel("Gandhi");
-		Hostel meera = new Hostel("Meera");
-		Hostel vyas = new Hostel("Vyas");
-		Hostel budh = new Hostel("Budh");
-		Hostel ram = new Hostel("Ram");
-		Admin admin = new Admin();
-
-		
-		
+		//to be implemented alongside gui
 	}
 }
