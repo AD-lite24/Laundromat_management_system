f5e3e3b68880abfad5847d912cf7a7809a739af1
Aditya Dandwate <adityadandwate24@gmail.com>
2022-12-02 21:27:30 +0530

    added login functionality

--- src/GUI/AdminWindow.java
+++ src/GUI/AdminWindow.java
@@ -62,7 +62,9 @@ public class AdminWindow extends JFrame {
 		contentPane.add(userName);
 		userName.setColumns(10);
 
-		
+		pass = new JPasswordField();
+		pass.setBounds(204, 130, 151, 25);
+		contentPane.add(pass);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -73,8 +75,8 @@ public class AdminWindow extends JFrame {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println(userName.getText());
-				System.out.println(lblPassword.getText());
-				boolean loginSuccess = LaundroSystem.admin.login(userName.getText(), lblPassword.getText());
+				System.out.println(String.valueOf(pass.getPassword()));
+				boolean loginSuccess = LaundroSystem.admin.login(userName.getText(), String.valueOf(pass.getPassword()));
 				if(loginSuccess) {
 					AdminFunctions af = new AdminFunctions();
 					af.setVisible(true);
@@ -92,9 +94,7 @@ public class AdminWindow extends JFrame {
 		btnNewButton.setBounds(143, 220, 117, 33);
 		contentPane.add(btnNewButton);
 		
-		pass = new JPasswordField();
-		pass.setBounds(204, 130, 151, 25);
-		contentPane.add(pass);
+
 		
 		lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setBounds(152, 173, 108, 25);
