bdad1314961be5ab517208ab3a5179c519433a05
Aditya Dandwate <adityadandwate24@gmail.com>
2022-12-05 00:50:13 +0530

    changes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String args[]) throws IOException, ClassNotFoundException {
 		
-		
+		System.out.println("Welcome to Laundromat");
 		LaundroSystem laundro = new LaundroSystem();
 		LaundroSystem.readFromFile();
 
