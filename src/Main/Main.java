ed9feaa56f0941f4f5b7f58261abc5cac8da5ea7
Aditya Dandwate <adityadandwate24@gmail.com>
2022-12-04 19:58:18 +0530

    stuff is working

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,7 +8,7 @@ import java.awt.EventQueue;
 public class Main {
 
 	public static void main(String args[]) throws IOException, ClassNotFoundException {
-		//to be implemented alongside gui
+		
 		LaundroSystem laundro = new LaundroSystem();
 		LaundroSystem.readFromFile();
 
