a7a9ffce9643ca90a962edf3958f05c913a4f4f2
Aditya Dandwate <adityadandwate24@gmail.com>
2022-12-04 16:31:05 +0530

    stuff

--- src/GUI/StudentWindow.java
+++ src/GUI/StudentWindow.java
@@ -12,7 +12,7 @@ import javax.swing.ButtonGroup;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class StudentWindow extends JFrame {
+public class StudentWindow extends JFrame{
 
 	private JPanel contentPane;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
